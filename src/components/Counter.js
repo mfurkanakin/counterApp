b06de71b68fa61da18d14b10127c6edc67beaff5
unknown <furkanakinmfa@gmail.com>
2023-09-28 17:23:36 +0300

    completed to project(add to state and dispatch)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,32 @@
-import React from 'react'
-import { useSelector} from 'react-redux'
+import { useState } from 'react';
+import { useSelector, useDispatch} from 'react-redux';
+import {increment, decrement, incrementByAmount, decrementByAmount} from '../redux/counter/counterSlice';
 
 function Counter() {
+    const [amount, setAmount] = useState(3);
     const countValue = useSelector((state) => state.counter.value);
-
-    console.log(countValue);
+    const dispatch = useDispatch();
     
   return (
-    <h1>{countValue}</h1>
-    )
+    <div>
+          <h1>{countValue}</h1>
+
+          <button onClick={() => dispatch(decrement())}>Decrement</button>
+          <button onClick={() => dispatch(increment())}>Increment</button>
+          <br/>
+          <br/>
+          <input 
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
+          />
+          <br/>
+          <button onClick={() => dispatch(incrementByAmount(amount))}>Increment By Amount</button>
+          <br/>
+          <button onClick={() => dispatch(decrementByAmount(amount))}>Decrement By Amount</button>
+
+    </div>
+    );
 }
 
 export default Counter;
